9c4ae6286945974c9730442b9238e6f938728316
braiscg <brais@viko.net>
2024-08-06 15:10:47 +0200

    indext.ts changes for description and introduction

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'GBFoods - Ghana Customers Chatbot',
-  description: 'Welcome! I\'m here to assist you with insights into consumer behavior and food product preferences in Ghana, particularly focusing on tomato paste and related products. My knowledge is derived from 22 detailed consumer interviews conducted in Accra and Kumasi. Whether you need concise summaries of consumer interviews, comparative analyses of tomato paste brands, or insights into market trends and consumer preferences, I\'m here to help. Feel free to ask any questions, and I\'ll provide you with accurate, relevant information to support your decision-making process.',
+  title: 'GBFoods - Customer Immersion Insights Chatbot',
+  description: 'Welcome! I\'m here to provide insights on consumer behavior and food product preferences in Ghana, focusing on cooking bases, aids, and condiments. My knowledge comes from 22 consumer immersions in Accra and Kumasi, covering both food vendors and household consumers. I can offer summaries, brand comparisons, and market trends across different consumer segments and product categories. Ask me anything, and I\'ll provide accurate, data-driven information based solely on these immersions to support your decision-making.',
   copyright: 'Kraz.ai',
   privacy_policy: '',
   default_language: 'en',
