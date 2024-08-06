74b3158b6ad454f42a78e744143a55a5df7307cb
braiscg <brais@viko.net>
2024-08-06 17:36:06 +0200

    Añadir package-lock.json  a .gitignore para comprobar que funciona el deploy.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,4 +8,6 @@ docker rm kraz_difyai_gb || true
 docker pull vikolab/kraz-difyai-gb:latest
 
 # Ejecutar el nuevo contenedor
-docker run -d --name kraz_difyai_gb -p 8502:3000 vikolab/kraz-difyai-gb:latest
+docker run -d --name kraz_difyai_gb -p 8502:3000 --env-file /home/viko/kraz-gbfoods/.env vikolab/kraz-difyai-gb:latest
+
+docker logs kraz_difyai_gb
